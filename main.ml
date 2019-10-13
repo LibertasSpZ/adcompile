2d347aed4fec6413404251b35f7a0ac653d98c69
Shaopeng <shaopeng@wireless-10-104-82-21.umd.edu>
2019-10-13 18:27:55 -0400

    added syntax for S 3.1 (normal), S 5.1 (non-det, parameterized).

--- main.ml
+++ main.ml
@@ -5,13 +5,26 @@ type parid = string
 type par = parid list
 (* The list of parameters  *)
 
+type evaledparid = float
+type evaledpar = evaledparid list
+type evalpar = par -> evaledpar
+(* evaluating a list of parameters, e.g: (theta_1,theta_2)\mapsto
+(0,5,0.7)  *)
+
+
+
 type qbit =
 | Qvar of qid
 
 type qlist = qbit list
 
+
+
+
+(* below: syntax for parameterized progs (\S 4.1) *)
 type unitary = 
 | Gate of uid * par
+(* The above: parameterized unitary, e.g U(theta_1, theta_2)*)
 
 type com =
 | Abort of qlist
@@ -35,9 +48,6 @@ let rec unparse_qlist ql : string =
      let uq = unparse_qbit q in
      let ul = unparse_qlist l in
      uq ^ "," ^ ul
-
-
-
 
 let rec unparse_par theta : string =
    match theta with
@@ -77,17 +87,150 @@ let rec unparse_com c : string =
      let q = unparse_qbit qb in
      let s1 = unparse_com u1 in
      let s2 = unparse_com u2 in
-     "case M(" ^ q ^ ") = 0 then " ^ s1 ^ "else" ^ s2
+     "case M(" ^ q ^ ") = 0 then " ^ s1 ^ "else " ^ s2
   | Bwhile (num, qb, u1) ->
      let nt = Printf.sprintf "%d" num in
      let q = unparse_qbit qb in
      let s1 = unparse_com u1 in
      "while^" ^ nt ^ " M(" ^ q ^ ")= 1 do " ^ s1   
+(* endof syntax for parameterized progs (\S 4.1) *)
 
-let test_p1 =
+
+(* below: syntax for UNparameterized progs (\S 3.1) *)
+
+type unparunitary = 
+| UnparGate of uid * evaledpar 
+ 
+(* The above:  e.g U(0.5, 0.7). *)
+
+ type uNcom = 
+| UNAbort of qlist
+| UNSkip of qlist
+| UNInit of qbit
+| UNUapp of unparunitary * qlist
+| UNSeq of uNcom * uNcom
+| UNCase of qbit * uNcom * uNcom
+| UNBwhile of int * qbit * uNcom 
+
+
+let rec unparse_evaledpar theta : string =
+   match theta with
+   | [] -> ""
+   | p :: [] -> Printf.sprintf "%5f" p
+   | p :: l ->  
+      let sl = unparse_evaledpar l in
+      Printf.sprintf "%5f" p ^ "," ^ sl
+
+
+let unparse_unparunitary u : string =
+  match u with
+  | UnparGate (g, t) -> 
+     let st = unparse_evaledpar t in 
+     g ^ "(" ^ st ^ ")"
+
+
+let rec unparse_UNcom c : string = 
+  match c with
+  | UNAbort ql -> 
+     let sql = unparse_qlist ql in
+     "Abort[" ^ sql ^ "]"
+  | UNSkip ql -> 
+     let sql = unparse_qlist ql in
+      "Skip[" ^ sql ^ "]"
+  | UNInit q -> 
+     let s = unparse_qbit q in 
+     s ^ ":=∣0⟩"
+  | UNUapp (u, ql) -> 
+     let su = unparse_unparunitary u in 
+     let sql = unparse_qlist ql in 
+     sql ^ ":=" ^ su ^ "[" ^ sql ^ "]"
+  | UNSeq (c1, c2) ->
+     let s1 = unparse_UNcom c1 in
+     let s2 = unparse_UNcom c2 in
+     s1 ^ "; " ^ s2
+  | UNCase (qb, u1, u2) ->
+     let q = unparse_qbit qb in
+     let s1 = unparse_UNcom u1 in
+     let s2 = unparse_UNcom u2 in
+     "case M(" ^ q ^ ") = 0 then " ^ s1 ^ "else " ^ s2
+  | UNBwhile (num, qb, u1) ->
+     let nt = Printf.sprintf "%d" num in
+     let q = unparse_qbit qb in
+     let s1 = unparse_UNcom u1 in
+     "while^" ^ nt ^ " M(" ^ q ^ ")= 1 do " ^ s1   
+(* endof syntax for UNparameterized progs (\S 3.1) *)
+
+
+
+(* below: syntax for non-deterministic parameterized progs (\S 5.1) *)
+type underlineUnitary = 
+| UnderlineGate of uid * par
+(* The above: parameterized unitary with non-det type (before compilation),
+ e.g \underline{U(theta_1, theta_2)} *)
+
+type underlineCom =
+| UnderlineAbort of qlist
+| UnderlineSkip of qlist
+| UnderlineInit of qbit
+| UnderlineUapp of underlineUnitary * qlist
+| UnderlineSeq of underlineCom * underlineCom
+| UnderlineCase of qbit * underlineCom * underlineCom
+| UnderlineBwhile of int * qbit * underlineCom 
+| UnderlineAdd of underlineCom * underlineCom
+
+
+
+let unparse_UnderlineUnitary u : string =
+  match u with
+  | UnderlineGate (g, t) -> 
+     let st = unparse_par t in 
+     g ^ "(" ^ st ^ ")"
+
+let rec unparse_UnderlineCom c : string = 
+  match c with
+  | UnderlineAbort ql -> 
+     let sql = unparse_qlist ql in
+     "___ Abort[" ^ sql ^ "] ___ "
+  | UnderlineSkip ql -> 
+     let sql = unparse_qlist ql in
+      "___ Skip[" ^ sql ^ "] ___ "
+  | UnderlineInit q -> 
+     let s = unparse_qbit q in 
+     "___" ^ s ^ ":=∣0⟩ ___ "
+  | UnderlineUapp (u, ql) -> 
+     let su = unparse_UnderlineUnitary u in 
+     let sql = unparse_qlist ql in 
+     "___" ^ sql ^ ":=" ^ su ^ "[" ^ sql ^ "] ___ "
+  | UnderlineSeq (c1, c2) ->
+     let s1 = unparse_UnderlineCom c1 in
+     let s2 = unparse_UnderlineCom c2 in
+     "___" ^ s1 ^ "; " ^ s2 ^ "___ "
+  | UnderlineCase (qb, u1, u2) ->
+     let q = unparse_qbit qb in
+     let s1 = unparse_UnderlineCom u1 in
+     let s2 = unparse_UnderlineCom u2 in
+     "___ case M(" ^ q ^ ") = 0 then " ^ s1 ^ "else " ^ s2 ^ "___ "
+  | UnderlineBwhile (num, qb, u1) ->
+     let nt = Printf.sprintf "%d" num in
+     let q = unparse_qbit qb in
+     let s1 = unparse_UnderlineCom u1 in
+     "___ while^" ^ nt ^ " M(" ^ q ^ ")= 1 do " ^ s1 ^ "___ "
+  | UnderlineAdd (u1, u2) -> 
+     let s1 = unparse_UnderlineCom u1 in
+     let s2 = unparse_UnderlineCom u2 in
+     "___" ^ s1 ^ "+" ^ s2 ^ "___ "
+(* endof syntax for parameterized progs (\S 5.1) *)
+
+
+
+(* Test cases: test_p2 for parameterized (\S 4.1), 
+test_p3 for unparameterized (\S 3.1),
+test_p4 for parameterized non-det (\S 5.1) *)
+
+(*  let test_p1 =
   let c1 = Init (Qvar "q0") in
   let c2 = (Uapp (Gate ("H", ["theta"]), [Qvar "q0"])) in
-  Seq(Seq(c1, c2), c2)
+  Seq(Seq(c1, c2), c2)  *)
 
 let test_p2 =
   let c0 = Init (Qvar "q0") in
@@ -98,14 +241,83 @@ let test_p2 =
   let guard = Qvar "q3" in 
   Bwhile (2, guard, c4)     
  
-(* Changed 0.5 and 0.3 to theta_1 and theta_2 *)
   
-let () =
+(* let () =
   let c = test_p2 in
   (* let c = Skip in  *)
   let s = unparse_com c in
   print_endline s
 
+Printf.sprintf "\r" *)
+
+let test_p3 =
+  let c0 = UNInit (Qvar "q0") in
+  let c1 = UNInit (Qvar "q1") in
+  let c2 = (UNUapp (UnparGate ("XX", [0.99]), [Qvar "q0"; Qvar "q1"])) in
+  let c3 = (UNUapp (UnparGate ("YY", [0.66]), [Qvar "q0"; Qvar "q1"])) in
+  let c4 = UNSeq(UNSeq(c0, c3), UNSeq(c2, c1)) in
+  let c5 = UNSeq(UNSeq(c2, c0), UNSeq(c1, c3)) in 
+  let guard = Qvar "q3" in 
+  UNCase (guard, c4, c5)
+
+  (* let () =
+  let c = test_p3 in
+  (* let c = Skip in  *)
+  let s = unparse_UNcom c in
+  print_endline s
+
+Printf.sprintf "\r"  *)
+
+let test_p4 =
+  let c0 = UnderlineInit (Qvar "q0") in
+  let c1 = UnderlineInit (Qvar "q1") in
+  let c2 = (UnderlineUapp (UnderlineGate ("X", ["theta_1"]), [Qvar "q0"])) in
+  let c3 = (UnderlineUapp (UnderlineGate ("Y", ["theta_2"]), [Qvar "q1"])) in
+  let c4 = UnderlineSeq(c1, c2) in
+  let c5 = UnderlineAdd(c0, c3) in 
+  let guard = Qvar "q3" in 
+  UnderlineCase (guard, c4, c5)
+
+  let () =  
+  let c2 = test_p2 in 
+  let s2 = unparse_com c2 in
+  print_endline s2
+
+  let () =
+  let cnextline = "\r" in 
+  print_endline cnextline
+
+  let () =  
+  let c3 = test_p3 in 
+  let s3 = unparse_UNcom c3 in
+  print_endline s3
+
+  let () =
+  let cnextline = "\r" in 
+  print_endline cnextline
+  
+  let () =  
+  let c4 = test_p4 in 
+  let s4 = unparse_UnderlineCom c4 in
+  print_endline s4
+
+  (* let () =  
+  let c2 = test_p2 in 
+  let s2 = unparse_com c2 in
+  
+  
+  Printf.sprintf "%s" "\r" *)
+
+  
+  (*let c2 = test_p2 in
+  let c3 = test_p3 in 
+  let c4 = test_p4 in
+  (* let c = Skip in  *)
+  
+  let s2 = unparse_com c2 in
+  let s3 = unparse_UNcom c3 in 
+  let s4 = unparse_UnderlineCom in
+  print_endline (* Printf.sprintf "%s"*)  s1  ^ "\r\r" ^ s2 ^ "\r\r" ^ s3 ^ "\r\r" ^ s4 *)
 
   (* TODO: We need to be assign values to parameters,
      so need a function type assign: parid -> float;
