46b94bcf2507c3edb3e38090f53fe1ff813e9b20
Shaopeng <shaopeng@Shaopengs-MacBook.local>
2019-11-12 10:27:28 -0500

    parser-compiler for abort, skip, unitary and sequence. Test: make, then rlwrap ./top, then Main.automate_qiuDao "testt.prog" "t1"

--- parser/main.ml
+++ parser/main.ml
@@ -12,10 +12,46 @@ let astqubit_to_str qub =
 			   let scum = "q" ^ snum in 
 			   Compiler.Qvar scum
 
+
+let astpar_to_str parb = 
+	match parb with
+| Ast.T num -> let snum = string_of_int(num) in 
+			   "t" ^ snum
+
+
 let rec astqubl_to_strl qubl = 
 	match qubl with
 | [] -> []	
 | (Ast.Q num) :: qubl2 -> (astqubit_to_str (Ast.Q num)) :: (astqubl_to_strl qubl2)
+
+let rec astparl_to_strl parl = 
+	match parl with
+| [] -> []	
+| (Ast.T num) :: parl2 -> (astpar_to_str (Ast.T num)) :: (astparl_to_strl parl2)
+
+
+
+let astU_to_compU unita = 
+	match unita with 
+| Ast.H parl -> Compiler.Gate("H",(astparl_to_strl parl))
+| Ast.CN parl -> Compiler.Gate("CNOT",(astparl_to_strl parl))
+| Ast.CX parl -> Compiler.Gate("C-X",(astparl_to_strl parl))
+| Ast.CY parl -> Compiler.Gate("C-Y",(astparl_to_strl parl))
+| Ast.CZ parl -> Compiler.Gate("C-Z",(astparl_to_strl parl))
+| Ast.CXX parl -> Compiler.Gate("C-XX",(astparl_to_strl parl))
+| Ast.CYY parl -> Compiler.Gate("C-YY",(astparl_to_strl parl))
+| Ast.CZZ parl -> Compiler.Gate("C-ZZ",(astparl_to_strl parl))
+| Ast.X parl -> Compiler.OneBRot("X",(astparl_to_strl parl))
+| Ast.Y parl -> Compiler.OneBRot("Y",(astparl_to_strl parl))
+| Ast.Z parl -> Compiler.OneBRot("Z",(astparl_to_strl parl))
+| Ast.XX parl -> Compiler.TwoBRot("XX",(astparl_to_strl parl))
+| Ast.YY parl -> Compiler.TwoBRot("YY",(astparl_to_strl parl))
+| Ast.ZZ parl -> Compiler.TwoBRot("ZZ",(astparl_to_strl parl))
+
+
+
+
+
 (* Need to convert the Parser qubit lists to compiler qubit 
 lists too. *)
 
@@ -27,6 +63,9 @@ lists too. *)
 let rec parser_to_compiler prog =
 	match prog with
 | Ast.Abort (a,b) -> Compiler.Abort ((* str_to_complql*)(astqubl_to_strl b)) 
+| Ast.Skip (a,b) -> Compiler.Skip ((* str_to_complql*)(astqubl_to_strl b)) 
+| Ast.Uapp (a,b) -> Compiler.Uapp ((astU_to_compU a), (astqubl_to_strl b))
+| Ast.Seq (a,b) -> Compiler.Seq ((parser_to_compiler a), (parser_to_compiler b))
 | _ -> Compiler.Abort [Compiler.Qvar "bj"]
 
 
